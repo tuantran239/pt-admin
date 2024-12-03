87b9f0864d12f1cb0f0cd7e99412be79b77799a4
tt239 <tuanphamtran99@gmail.com>
2024-12-03 21:37:13 +0700

    Setup project

--- deploy/scripts/notification.sh
+++ deploy/scripts/notification.sh
@@ -1,7 +1,7 @@
 if [ $BUILD_FAIL == false ]; then
    echo "🟢 success" 
-   curl -X POST -H 'Content-type: application/json' --data '{"text":"🟢 Deploy success"}' https://hooks.slack.com/services/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    
+   curl -X POST -H 'Content-type: application/json' --data '{"text":"🟢 Deploy to Digital Ocean success"}' $SLACK_WEBHOOK    
 else
    echo "🔴 fail"  
-   curl -X POST -H 'Content-type: application/json' --data '{"text":"🔴 Deploy fail"}' https://hooks.slack.com/services/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  
+   curl -X POST -H 'Content-type: application/json' --data '{"text":"🔴 Deploy to Digital Ocean success"}' $SLACK_WEBHOOK  
 fi
