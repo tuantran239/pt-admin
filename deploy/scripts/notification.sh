0c477c472f2d00e61c21ff9a26f5031b8b90890c
tt239 <tuanphamtran99@gmail.com>
2024-12-06 14:37:45 +0700

    Set up CICD deploy

--- deploy/scripts/notification.sh
+++ deploy/scripts/notification.sh
@@ -3,5 +3,5 @@ if [ $BUILD_FAIL == false ]; then
    curl -X POST -H 'Content-type: application/json' --data '{"text":"🟢 Deploy Refind Admin to Digital Ocean success"}' $SLACK_WEBHOOK    
 else
    echo "🔴 fail"  
-   curl -X POST -H 'Content-type: application/json' --data '{"text":"🔴 Deploy Refind Admin to Digital Ocean success"}' $SLACK_WEBHOOK  
+   curl -X POST -H 'Content-type: application/json' --data '{"text":"🔴 Deploy Refind Admin to Digital Ocean failed"}' $SLACK_WEBHOOK  
 fi
